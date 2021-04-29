1fe4374a2a6a335ff92bc1971bb18234cfd567d8
jrsalazarleon <jrsalazarleon@gmail.com>
2021-04-28 21:27:19 -0400

    Remove images

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const slide = document.getElementById("#slide");
-let slideElement = document.querySelectorAll(".slide-img");
-let slideElementLast = slideElement[slideElement.length -1];
-const buttonNext = document.getElementById("button-next");
-const buttonPrev = document.getElementById("button-prev")
-
-slide.insertAdjacentElement("afterbegin", slideElementLast)
-
